33e32e0b976ff708ba2ca8b2aef3b6a44ac563c4
helgrind-cyber <helgrindxxx@gmail.com>
2020-03-25 20:54:28 +0700

    second commit 25/03

--- booking-hotel.sql
+++ booking-hotel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 23, 2020 at 10:00 AM
+-- Generation Time: Mar 25, 2020 at 08:53 PM
 -- Server version: 5.7.29-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.3
+-- PHP Version: 7.3.16-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -54,15 +54,24 @@ CREATE TABLE `booking` (
 CREATE TABLE `contacts` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `subject` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `messages` text COLLATE utf8_unicode_ci NOT NULL,
   `status` int(1) NOT NULL,
-  `reply_by` int(11) NOT NULL,
-  `reply_for` int(11) NOT NULL,
-  `create_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `reply_by` int(11) DEFAULT NULL,
+  `reply_for` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `reply_messages` text COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `phone_number`, `email`, `subject`, `messages`, `status`, `reply_by`, `reply_for`, `created_at`, `reply_messages`) VALUES
+(1, 'customer1', '0868456456', 'customer1@gmail.com', 'Phản hồi vu vơ 1', 'Nội dung 1', 1, 1, 1, NULL, 'Nội dung phản hồi customer 1'),
+(2, 'customer2', '0123456789', 'customer2@gmail.com', 'Phản hồi vu vơ 2', 'Nội dung phản hồi vu vơ 2', 1, 1, 2, NULL, 'Nội dung phản hồi khách hàng 2');
 
 -- --------------------------------------------------------
 
@@ -364,7 +373,7 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `customer_feedbacks`
 --
