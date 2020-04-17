8665282d493029528b875049d4695d74cda8ca04
helgrind-cyber <helgrindxxx@gmail.com>
2020-04-17 17:02:50 +0700

    Change customer feedbacks code

--- admin/customer_feedbacks/edit-form.php
+++ admin/customer_feedbacks/edit-form.php
@@ -3,11 +3,11 @@ session_start();
 require_once '../../config/utils.php';
 checkAdminLoggedIn();
 
-// lấy thông tin của người dùng ra ngoài thông id trên đường dẫn
+// get id from url
 $id = isset($_GET['id']) ? $_GET['id'] : -1;
 /// get customer feedbacks query
 $getFeedbacksQuery = "select * from customer_feedbacks where id = '$id'";
-$feedbacks = queryExecute($getFeedbacksQuery, false);
+$feedback = queryExecute($getFeedbacksQuery, false);
 ?>
 <!DOCTYPE html>
 <html>
@@ -53,24 +53,24 @@ $feedbacks = queryExecute($getFeedbacksQuery, false);
                     <form id="add-feedbacks-form" action="<?= ADMIN_URL . 'customer_feedbacks/save-edit.php' ?>" method="post" enctype="multipart/form-data">
                         <div class="row">
                             <div class="col-md-6">
-                                <input type="text" name="id" value="<?= $feedbacks['id'] ?>" hidden>
+                                <input type="hidden" name="id" value="<?= $feedback['id'] ?>">
                                 <div class="form-group">
                                     <label for="">Tên người dùng<span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" name="name" value="<?= $feedbacks['name'] ?>">
+                                    <input type="text" class="form-control" name="name" value="<?= $feedback['name'] ?>">
                                     <?php if (isset($_GET['nameerr'])) : ?>
                                         <label class="error"><?= $_GET['nameerr'] ?></label>
                                     <?php endif; ?>
                                 </div>
                                 <div class="form-group">
                                     <label for="">Địa chỉ<span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" name="address" value="<?= $feedbacks['address'] ?>">
+                                    <input type="text" class="form-control" name="address" value="<?= $feedback['address'] ?>">
                                     <?php if (isset($_GET['addresserr'])) : ?>
                                         <label class="error"><?= $_GET['addresserr'] ?></label>
                                     <?php endif; ?>
                                 </div>
                                 <div class="form-group">
                                     <label for="">Nội dung<span class="text-danger">*</span></label>
-                                    <textarea name="content" class="form-control" id="content-feedback">value="<?= $feedbacks['content'] ?>"</textarea>
+                                    <textarea name="content" class="form-control" rows="5" id="content-feedback"><?= $feedback['content'] ?></textarea>
                                     <?php if (isset($_GET['contenterr'])) : ?>
                                         <label class="error"><?= $_GET['contenterr'] ?></label>
                                     <?php endif; ?>
@@ -80,28 +80,25 @@ $feedbacks = queryExecute($getFeedbacksQuery, false);
                             <div class="col-md-6">
                                 <div class="row">
                                     <div class="col-md-6 offset-md-3">
-                                        <img src="<?= BASE_URL . $feedbacks['avatar'] ?>" id="preview-img" class="img-fluid">
+                                        <img src="<?= BASE_URL . $feedback['avatar'] ?>" id="preview-img" class="img-fluid">
                                     </div>
                                 </div>
-                                <div class="input-group mt-3 mb-4">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Ảnh đại diện<span class="text-danger">*</span></span>
-                                    </div>
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="inputGroupFile01" name="avatar" onchange="encodeImageFileAsURL(this)">
-                                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                                <div class="form-group mt-3 mb-4">
+                                    <div class="form-group">
+                                        <label for="">Ảnh đại diện<span class="text-danger">*</span></label>
+                                        <input type="file" class="form-control-file" name="avatar" onchange="encodeImageFileAsURL(this)">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="mySelect1">Trạng thái<span class="text-danger">*</span></label>
                                     <select id="mySelect1" class="form-control" name="status">
                                         <?php
-                                        if ($feedbacks['status'] == 1) { ?>
-                                            <option value="1" selected>Active</option>
-                                            <option value="0">Inactive</option>
-                                        <?php    } else if ($feedbacks['status'] == 0) { ?>
-                                            <option value="1">Active</option>
-                                            <option value="0" selected>Inactive</option>
+                                        if ($feedback['status'] == 1) { ?>
+                                            <option value="<?= ACTIVE?>" selected>Họat động</option>
+                                            <option value="<?= INACTIVE?>">Ngừng hoạt động</option>
+                                        <?php    } else if ($feedback['status'] == 0) { ?>
+                                            <option value="<?= ACTIVE?>">Họat động</option>
+                                            <option value="<?= INACTIVE?>" selected>Ngừng hoạt động</option>
                                         <?php } ?>
                                     </select>
                                 </div>
@@ -129,7 +126,7 @@ $feedbacks = queryExecute($getFeedbacksQuery, false);
         function encodeImageFileAsURL(element) {
             var file = element.files[0];
             if (file === undefined) {
-                $('#preview-img').attr('src', "<?= BASE_URL . $user['avatar'] ?>");
+                $('#preview-img').attr('src', "<?= DEFAULT_IMAGE ?>");
                 return false;
             }
             var reader = new FileReader();
@@ -138,58 +135,47 @@ $feedbacks = queryExecute($getFeedbacksQuery, false);
             }
             reader.readAsDataURL(file);
         }
-        $('#edit-user-form').validate({
+        $('#add-feedbacks-form').validate({
             rules: {
                 name: {
                     required: true,
+                    minlength: 2,
                     maxlength: 191
                 },
-                email: {
+                address: {
+                    required: true
+                },
+                content: {
                     required: true,
-                    maxlength: 191,
-                    email: true,
-                    remote: {
-                        url: "<?= ADMIN_URL . 'users/verify-email-existed.php' ?>",
-                        type: "post",
-                        data: {
-                            email: function() {
-                                return $("input[name='email']").val();
-                            },
-                            id: <?= $user['id']; ?>
-                        }
-                    }
-                },
-                phone_number: {
-                    number: true
-                },
-                house_no: {
-                    maxlength: 191
+                    minlength: 2
                 },
                 avatar: {
+                    required: true,
                     extension: "png|jpg|jpeg|gif"
+                },
+                status: {
+                    required: true
                 }
             },
             messages: {
                 name: {
                     required: "Hãy nhập tên người dùng",
+                    minlength: "Số lượng ký tự tối thiểu bằng 2",
                     maxlength: "Số lượng ký tự tối đa bằng 191 ký tự"
                 },
-                email: {
-                    required: "Hãy nhập email",
-                    maxlength: "Số lượng ký tự tối đa bằng 191 ký tự",
-                    email: "Không đúng định dạng email",
-                    remote: "Email đã tồn tại, vui lòng sử dụng email khác"
+                address: {
+                    required: "Hãy nhập địa chỉ người dùng"
                 },
-                phone_number: {
-                    min: "Bắt buộc là số có 10 chữ số",
-                    max: "Bắt buộc là số có 10 chữ số",
-                    number: "Nhập định dạng số"
-                },
-                house_no: {
-                    maxlength: "Số lượng ký tự tối đa bằng 191 ký tự"
+                content: {
+                    required: "Hãy nhập nội dùng phản hồi",
+                    minlength: "Không thể nhập nội dung dưới 2 ký tự"
                 },
                 avatar: {
+                    required: "Hãy nhập ảnh đại diện",
                     extension: "Hãy nhập đúng định dạng ảnh (jpg | jpeg | png | gif)"
+                },
+                status: {
+                    required: "Chọn trạng thái hoạt động"
                 }
             }
         });
--- admin/customer_feedbacks/save-add.php
+++ admin/customer_feedbacks/save-add.php
@@ -2,29 +2,39 @@
 session_start();
 include_once "../../config/utils.php";
 checkAdminLoggedIn();
+
 $name = trim($_POST['name']);
 $address = trim($_POST['address']);
 $content = trim($_POST['content']);
-
 $status = trim($_POST['status']);
 $avatar = $_FILES['avatar'];
 // validate bằng php
 $nameerr = "";
 $addresserr = "";
+$contenterr = "";
 // check name
 if (strlen($name) < 2 || strlen($name) > 191) {
     $nameerr = "Yêu cầu nhập tên tài khoản nằm trong khoảng 2-191 ký tự";
 }
 
-if ($nameerr != "") {
-    header('location: ' . ADMIN_URL . "customer_feedbacks/add-form.php?nameerr=$nameerr");
+// if (!preg_match('/^[^@]{1,63}@[^@]{1,255}$/', $address)) {
+//     $addresserr = "Nhập đúng định dạng địa chỉ";
+// }
+// keyword regex
+
+if(strlen($content) < 2) {
+    $contenterr = "Không thể nhập nội dung dưới 2 ký tự";
+}
+
+if ($nameerr . $addresserr . $contenterr != "") {
+    header('location: ' . ADMIN_URL . "customer_feedbacks/add-form.php?nameerr=$nameerr&addresserr=$addresserr&contenterr=$contenterr");
     die;
 }
 
 // upload file ảnh
 $filename = "";
 if($avatar['size'] > 0){
-    $filename = uniqid() . '-' . $avatar['name'];
+    $filename = uniqid() . 'feedback -' . $avatar['name'];
     move_uploaded_file($avatar['tmp_name'], "../../public/img/" . $filename);
     $filename = "public/img/" . $filename;
 }
@@ -33,7 +43,6 @@ $insertFeedbacksQuery = "insert into customer_feedbacks
                           (name, address, avatar, content, status)
                     values
                           ('$name', '$address', '$filename', '$content', '$status')";
-// dd($insertFeedbacksQuery);
 queryExecute($insertUserQuery, false);
 header("location: " . ADMIN_URL . "customer_feedbacks");
 die;
