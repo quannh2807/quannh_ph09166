41a6b403b020919c9eb028c0146975a8552cd9d5
helgrind-cyber <helgrindxxx@gmail.com>
2020-04-23 19:32:33 +0700

    Update feature booking in admin not use

--- public/_share/link.php
+++ public/_share/link.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title><?=$title?></title>
+<title><?= $title ?></title>
 <link rel="shortcut icon" href="<?= PUBLIC_URL ?>/img/favicon.ico" type="image/x-icon">
 <!-- fonts Roboto -->
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
@@ -28,3 +28,8 @@
 <link rel="stylesheet" href="<?= PUBLIC_URL ?>/css/responsive.css" />
 <!-- Sweet alert -->
 <link rel='stylesheet' href='https://cdn.rawgit.com/t4t5/sweetalert/v0.2.0/lib/sweet-alert.css'>
+<style>
+    .mainmenu .nav.navbar-nav li.dropdown>a {
+        background: url(<?=PUBLIC_URL . 'img/dropdown_bg.png'?>) no-repeat scroll 100% 50%;
+    }
+</style>
